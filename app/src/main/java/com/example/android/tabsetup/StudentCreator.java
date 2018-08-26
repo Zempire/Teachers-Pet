4e10edfc7fae533be5977d5136f965c7a25abbfc
Desktop <ginozem@gmail.com>
2018-08-27 00:10:36 +1000

    More Student Creator touch ups.

--- app/src/main/java/com/example/android/tabsetup/StudentCreator.java
+++ app/src/main/java/com/example/android/tabsetup/StudentCreator.java
@@ -2,19 +2,33 @@ package com.example.android.tabsetup;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import static android.R.layout.simple_spinner_dropdown_item;
 
 public class StudentCreator extends AppCompatActivity {
 
     TextView address;
     LinearLayout addressExpanded;
+    Spinner spinner;
+
+    String[] states = new String[]{
+            "State", "NSW", "VIC", "QLD", "NT", "WA", "SA", "TAS", "ACT"};
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_student);
 
@@ -29,5 +43,28 @@ public class StudentCreator extends AppCompatActivity {
                     addressExpanded.setVisibility(view.VISIBLE);
             }
         });
+
+        spinner = findViewById(R.id.stateSpinner);
+        addItemsToSpinner(spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    public void addItemsToSpinner(Spinner spinner) {
+        List<String> stateList = new ArrayList<>(Arrays.asList(states));
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item, stateList);
+        dataAdapter.setDropDownViewResource(simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
     }
 }
